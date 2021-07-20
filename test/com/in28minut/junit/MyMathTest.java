a29645422dd1e94dc2b7cc46d9062e75440c2919
Himanshu Mishra <himanshu.nit.mca@gmail.com>
2021-07-20 15:51:23 +0530

    important annotations related to JUnit

--- test/com/in28minut/junit/MyMathTest.java
+++ test/com/in28minut/junit/MyMathTest.java
@@ -2,11 +2,38 @@ package com.in28minut.junit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 
 class MyMathTest {
 //MyMath.sum
 	//1,2,3 => 6
+	@Before 
+	public void before() {
+		System.out.println("Before");
+	}
+	
+	
+	
+	@After
+	public void after() {
+		System.out.println("after");
+	}
+	
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("Before class");
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		System.out.println("After class");
+	}
+	
+	
 	@Test
 	void sumTest() {
 		MyMath myMath = new MyMath();
@@ -14,10 +41,6 @@ class MyMathTest {
 		//check that the res is 6
 		//check res == 6
 		assertEquals(6, res);
-	
-		
-		
-		
 		//ABsance of failure is success
 	}
 
